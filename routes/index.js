9f57fd9eae5cd452a64bade25be7523ae9af175a
James Postrasija <jamespostrasija@Jamess-iMac.hitronhub.home>
2019-09-22 14:12:24 -0600

    order-food

--- routes/index.js
+++ routes/index.js
@@ -312,9 +312,6 @@ router.delete("/users/:user_id", middleware.checkProfileOwnership, function(
     } else {
       try {
         await cloudinary.v2.uploader.destroy(user.imageId);
-        user.reviews.remove();
-        user.comments.remove();
-        user.lots.remove();
         user.remove();
         res.redirect("/");
       } catch (err) {
